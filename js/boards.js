f53698948814451fd355e0ba2f87ba5d01d649f1
¨Mario <mario.vizcainos@gmail.com>
2016-03-29 22:53:11 -0500

    feed boards data from the api

--- js/boards.js
+++ js/boards.js
@@ -1,2 +1,19 @@
 $( document ).ready(function() {
+  com.list("boards","",board.list)
 });
+
+function Board(){
+ var  x = this;
+ x.list = function (response){
+   var list = "";
+   for (i =0; i< response.boards.length; i++){
+     console.log(response.boards[i]);
+     list = list + '<button type="button" onclick="board.goTo('+response.boards[i].id+')" class="list-group-item">'+response.boards[i].name+'</button>'
+   }
+   $( "#boardsList" ).html(list);
+ }
+ x.goTo = function(id) {
+   alert(id);
+ }
+}
+board = new Board();
