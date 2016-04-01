3a22c8027d28401a1a4b6624efc0071aac21cf8d
Mario Vizcaino <mario.vizcainos@gmail.com>
2016-04-01 10:07:22 -0500

    test

--- js/boards.js
+++ js/boards.js
@@ -5,12 +5,13 @@ $( document ).ready(function() {
 function Board(){
  var  x = this;
  x.list = function (response){
-   var list = "";
+   var html = '<h1 id="Header" class="text-center">Select The Board</h1><div id="dataList" class="list-group">';
    for (i =0; i< response.boards.length; i++){
      console.log(response.boards[i]);
-     list = list + '<button type="button" onclick="board.goTo('+response.boards[i].id+')" class="list-group-item">'+response.boards[i].name+'</button>'
+     html = html + '<button type="button" onclick="board.goTo('+response.boards[i].id+')" class="list-group-item">'+response.boards[i].name+'</button>'
    }
-   $( "#boardsList" ).html(list);
+   html = html +  "</div>"
+   $( ".boardsContainer" ).html(html);
  }
  x.goTo = function(id) {
    alert(id);
