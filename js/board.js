3a285c3d2d5251c23eb430f8e853000ec17bc146
¨Mario <mario.vizcainos@gmail.com>
2016-04-02 10:11:34 -0500

    added new desing to the views

--- js/board.js
+++ js/board.js
@@ -7,17 +7,15 @@ var Board = {
 
 
 			$( ".boardsContainer" ).append( createElement("h1","Select a Board","", "text-center") );
-			$( ".boardsContainer" ).append( createElement("div","","dataList", "list-group") );
-
+			$( ".boardsContainer" ).append( createElement("div","","dataList", "mdl-grid") );
 			for (i =0; i< response.boards.length; i++){
-				var board = response.boards[i];
-				var button =  createElement("button",response.boards[i].name,"","board-button list-group-item list-item");
-				button.on('click', (function(boardCopy) {
-					return function() {
-						Board.goTo(boardCopy.name);
-					}
-				})(board));
-				$( "#dataList" ).append( button );
+				var html = ' <div class="card-square mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">';
+				var html = html + '<div class="mdl-card__title mdl-card--expand">';
+				var html = html + '<h2 class="mdl-card__title-text">'+ response.boards[i].name+'</h2></div>';
+				var html = html + '<div class="mdl-card__supporting-text">' + "I AM A DESCRIPTION"+ '</div>';
+				var html = html + '<div class="mdl-card__actions mdl-card--border">';
+				var html = html + '<a class="boardButton mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">View Updates </a></div></div><br>'
+				$("#dataList").append(html);
 			}
 		});
 
