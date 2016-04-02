9ff282eb2d21a2a2f66d89b2da4962d291014c27
¨Mario <mario.vizcainos@gmail.com>
2016-04-02 11:50:21 -0500

    added post list functions

--- js/post.js
+++ js/post.js
@@ -5,17 +5,21 @@ var Post = {
 		$.get( url + name, function(response) {
 			for (i = 0; i < response.post.length; i++) {
 				var post = response.post[i];
-				console.log(post);
-				html = '<div class="mdl-cell mdl-cell--6-col mdl-cell--2-col-tablet mdl-cell--4-col-phone">';
+				html = '<div class="mdl-cell mdl-cell--6-col mdl-cell--9-col-tablet mdl-cell--4-col-phone">';
 				html += '<article class="material-card">';
 				html += '<header><div class="material-card-icon"></div>';
 				html += '<h2 class="material-card-title">'+post.title+'</h2>';
 				html += '<div class="material-card-action">';
-				html += '<a href="#"> <i class="right-arrow material-icons md-36">keyboard_arrow_right</i></a></div>';
-				html += '<div class="material-card-info"><i class="material-icons">access_time</i><time>Created: '+post.created_at+'</time></div></header></article><br>';
+				html += '<a id="post_'+post.id+'" class="postButton" href="javascript:void(0);"> <i class="right-arrow material-icons md-36">keyboard_arrow_right</i></a></div>';
+				html += '<div class="material-card-info mdl-cell--hide-phone"><i class="material-icons">access_time</i><time>Created: '+post.created_at+'</time></div></header></article><br>';
 				html += '</div>';
 				$("#dataList").append(html);
     	}
+			$('.postButton').on('click', function() {
+				var data = this.id;
+				var arr = data.split('_');
+				Comment.list(arr[1],$('#currentBoard').val());
+			});
 		});
 	}
 }
