384b0a8452887b10038c58212250bb19b54e76c9
¨Mario <mario.vizcainos@gmail.com>
2016-04-02 01:23:28 -0500

    update in post desing

--- js/post.js
+++ js/post.js
@@ -1,19 +1,27 @@
 var Post = {
 	list: function (name){
 		$( ".boardsContainer" ).append( createElement("h1","All the post in " + name, "", "text-center") );
-		$( ".boardsContainer" ).append( createElement("div","","dataList", "list-group") );
+		$( ".boardsContainer" ).append( createElement("div","","dataList", "") );
 
 		$.get( url + name, function(response) {
 			for (i = 0; i < response.post.length; i++) {
+
 				var post = response.post[i];
 				console.log(post);
-				var button = createElement("button", post.title,"","post-button list-group-item list-item");
+        var div = createElement("div","","", "postContainer center-block");
+        var header  = createElement("div","","", "postHeader");
+        var date = createElement("div",post.created_at,"", "postDate");
+        var h6 = createElement("h6",post.title,"", "");
+        var p  = createElement("p","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","", "");
+        header.append(h6,date);
+        div.append(header,p);
+        var button = createElement("button", post.title,"","post-button");
 				button.on('click', (function(postCopy) {
 					return function() {
 						Comment.list(postCopy.id,name);
 					}
 				})(post));
-				$( "#dataList" ).append( button );
+				$( "#dataList" ).append( div, "<br>" );
 			}
 		});
 	}
