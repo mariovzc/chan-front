72d66f634f855f7c5437f4193eef4f77f9b481f5
¨Mario <mario.vizcainos@gmail.com>
2016-04-02 00:54:13 -0500

    added board.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,7 @@
 var url = "http://chanapi.herokuapp.com/";
 var pos;
-var Board = {
-	list : function (){
-		clear();
-		$( "#addButton" ).addClass( "hidden" );
-		$.get( url + "boards", function( response ) {
-			var dataList = $( document.createElement('div') );
-
-
-			$( ".boardsContainer" ).append( createElement("h1","Select a Board","", "text-center") );
-			$( ".boardsContainer" ).append( createElement("div","","dataList", "list-group") );
-
-			for (i =0; i< response.boards.length; i++){
-				var board = response.boards[i];
-				var button =  createElement("button",response.boards[i].name,"","board-button list-group-item list-item");
-				button.on('click', (function(boardCopy) {
-					return function() {
-						Board.goTo(boardCopy.name);
-					}
-				})(board));
-				$( "#dataList" ).append( button );
-			}
-		});
-
-	},
-	goTo : function(name){
-		clear();
-		$( "#addButton" ).removeClass( "hidden" );
-		Post.list(name);
-		pos = 1;
-	}
-};
-
 Board.list();
+
 function createElement(element,text,id,clss){
 	var element = $(document.createElement(element));
 	if (text)
@@ -46,12 +15,15 @@ function createElement(element,text,id,clss){
 
 	return element;
 }
+
 function clear(){
 	$(".boardsContainer").html("");
 }
+
 $( "#mainIcon" ).click(function() {
 	Board.list();
 });
+
 $( "#addButton" ).click(function() {
 	$('.modal').modal('toggle');
 	var titulo = $(".modal-title");
