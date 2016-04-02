26dd4dfcf3cf7a22f863ff7832a315cc2352f6e2
¨Mario <mario.vizcainos@gmail.com>
2016-04-02 12:02:14 -0500

    some bugs in design fixed, added comment methods

--- js/comment.js
+++ js/comment.js
@@ -2,10 +2,10 @@ var Comment = {
 	list: function (post,name){
 		clear();
 		pos = 2;
+		$( ".boardsContainer" ).append( createElement("div","","dataList", "content-grid mdl-grid") );
 		$.get(url + name + "/" + post, function(response){
 			for (i=0; i < response.comments.length; i++){
-				comment = response.comments[i];
-				console.log(comment);
+								
 			}
 		});
 	}
